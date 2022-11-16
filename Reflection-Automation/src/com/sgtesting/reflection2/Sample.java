8f7adf6f219113fc10ff780a49664bbf788bd09a
Taabaasuma <tabasumamakarabbi@gmail.com>
2022-11-16 10:38:22 +0530

    Reflection

---
+++ Reflection-Automation/src/com/sgtesting/reflection2/Sample.java
@@ -0,0 +1,54 @@
+package com.sgtesting.reflection2;
+
+public class Sample {
+
+	public void display1()
+	{
+		System.out.println("This is a Display1 Method Statement");
+	}
+	
+	public void display2()
+	{
+		System.out.println("This is a Display2 Method Statement");
+	}
+	
+	public void display3()
+	{
+		System.out.println("This is a Display3 Method Statement");
+	}
+	
+	public void display4()
+	{
+		System.out.println("This is a Display4 Method Statement");
+	}
+	
+	public void display5()
+	{
+		System.out.println("This is a Display5 Method Statement");
+	}
+	
+	public void display6()
+	{
+		System.out.println("This is a Display6 Method Statement");
+	}
+	
+	public void display7()
+	{
+		System.out.println("This is a Display7 Method Statement");
+	}
+	
+	public void display8()
+	{
+		System.out.println("This is a Display8 Method Statement");
+	}
+	
+	public void display9()
+	{
+		System.out.println("This is a Display9 Method Statement");
+	}
+	
+	public void display10()
+	{
+		System.out.println("This is a Display10 Method Statement");
+	}
+}
